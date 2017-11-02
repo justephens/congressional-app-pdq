2e28556f533a94a41bdfbbc902af1c63e1908c4e
dchangela <12160187@plps-12160187.pinelakeprep.org>
2017-11-02 10:17:47 -0400

    created log in page

---
+++ PDQ/app/src/main/java/plp/pdq/LoginActivity.java
@@ -0,0 +1,43 @@
+package plp.pdq;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText textUsername;
+    Button btnLogin, btnGoogle, btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        textUsername = (EditText) findViewById(R.id.UsernameLogin);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
+        btnCreateAccount = (Button) findViewById(R.id.buttonCreateAccount);
+        //btnGoogle = (Button) findViewById(R.id.buttonGoogle);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
