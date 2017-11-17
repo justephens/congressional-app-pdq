e011d37eac861632322f1366fd78b37ab1d1fd00
Justin Stephens <jstephens2020@outlook.com>
2017-11-17 10:19:33 -0500

    Added a payment activity to the settings

--- PDQ/app/src/main/java/plp/pdq/SettingsActivity.java
+++ PDQ/app/src/main/java/plp/pdq/SettingsActivity.java
@@ -1,13 +1,27 @@
 package plp.pdq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button btnPayOpt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        btnPayOpt = (Button) findViewById(R.id.buttonPaymentOpt);
+
+        btnPayOpt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity (new Intent(SettingsActivity.this, PaymentActivity.class));
+            }
+        });
     }
 }
